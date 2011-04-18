ca5d4cdcf793b968267e59a0eb2e43b0cdce1bcf
Alexandre Gandra <gandra@gmail.com>
2011-04-18 02:25:02 -0300

    Get or post works

--- sing.rb
+++ sing.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'haml'
 
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
+
 get '/favicon.ico' do
   [404, nil, '']
 end
 
-get '/:song' do |song|
+get_or_post '/:song' do |song|
   @song = song
   @output = `$HOME/.wit/#{song}` if song && song.match("^[\\w_\\-]+$")
   haml $?.success? ? :success : :fail
