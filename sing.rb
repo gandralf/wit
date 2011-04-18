170c354d2ee127ca553e3585e3bce9d6d35ef2b1
Alexandre Gandra <gandra@gmail.com>
2011-04-17 22:43:14 -0300

    wtf

--- sing.rb
+++ sing.rb
@@ -1,4 +1,30 @@
 require 'sinatra'
-get '/cdo.tv' do
-  system 'echo hello'
+require 'haml'
+
+get '/favicon.ico' do
+  [404, nil, '']
 end
+
+get '/:song' do |song|
+  @song = song
+  @output = `$HOME/.wit/#{song}` if song && song.match("^[\\w_\\-]+$")
+  haml $?.success? ? :success : :fail
+end
+
+__END__
+
+@@ layout
+%html
+  = yield
+  %div
+    Command: 
+    %pre #{@song}
+  %h2 Output
+  %pre #{@output}
+  Done.
+
+@@ success
+%h1 Aplause!
+
+@@ fail
+%h1 Buuuuu!
