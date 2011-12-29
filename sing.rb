c6c3624c85eb1dc50394ad97fc9122b164e08a20
Alexandre Gandra <gandra@gmail.com>
2011-12-29 16:50:27 -0300

    Con log

--- sing.rb
+++ sing.rb
@@ -10,10 +10,31 @@ get '/favicon.ico' do
   [404, nil, '']
 end
 
+get_or_post '/' do
+  @output = "Who is gonna sing?"
+  haml :success
+end
+
 get_or_post '/:song' do |song|
   @song = song
-  @output = `$HOME/.wit/#{song}` if song && song.match("^[\\w_\\-]+$")
-  haml $?.success? ? :success : :fail
+  if song && song.match("^[\\w_\\-]+$")
+    @output = `$HOME/.wit/#{song} &> log/#{song}.log && cat log/#{song}.log`
+    haml $?.success? ? :success : :fail
+  else
+    @output = "Invalid integration script: #{song}"
+    haml :fail
+  end
+end
+
+get_or_post '/:song/log' do |song|
+  @song = song
+  @output = ""
+  file = File.new("log/#{song}.log", "r")
+  while (line = file.gets)
+    @output += "#{line}"
+  end
+  file.close
+  haml file ? :success : :fail
 end
 
 __END__
